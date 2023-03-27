a4b13bc75718dff96fbfe0e2772c63786faff827
Geomiruch <popodyak@gmail.com>
2023-03-27 22:35:19 +0300

    Add first two hometasks

---
+++ Home_task_1/Task1_1/Task1_1/Program.cs
@@ -0,0 +1,112 @@
+﻿static void FillMatrixInClockOrder(int[,] matrix)
+{
+    int rowCount = matrix.GetLength(0);
+    int colCount = matrix.GetLength(1);
+    int maxRow = rowCount - 1, maxCol = colCount - 1;
+    int minRow = 0, minCol = 0;
+    int num = 1;
+
+    while (num <= rowCount * colCount)
+    {
+        for (int i = minCol; i <= maxCol; i++)
+        {
+            matrix[minRow, i] = num++;
+        }
+        minRow++;
+        for (int i = minRow; i <= maxRow; i++)
+        {
+            matrix[i, maxCol] = num++;
+        }
+        maxCol--;
+        if (num <= rowCount * colCount)
+        {
+            for (int i = maxCol; i >= minCol; i--)
+            {
+                matrix[maxRow, i] = num++;
+            }
+            maxRow--;
+        }
+        if (num <= rowCount * colCount)
+        {
+            for (int i = maxRow; i >= minRow; i--)
+            {
+                matrix[i, minCol] = num++;
+            }
+            minCol++;
+        }
+    }
+}
+
+static void FillMatrixInAntiClockOrder(int[,] matrix)
+{
+    int rowCount = matrix.GetLength(0);
+    int colCount = matrix.GetLength(1);
+    int maxRow = rowCount - 1, maxCol = colCount - 1;
+    int minRow = 0, minCol = 0;
+    int num = 1;
+
+    while (num <= rowCount * colCount)
+    {
+        for (int i = minRow; i <= maxRow; i++)
+        {
+            matrix[i, minCol] = num++;
+        }
+        minCol++;
+        for (int i = minCol; i <= maxCol; i++)
+        {
+            matrix[maxRow, i] = num++;
+        }
+        maxRow--;
+        if (num <= rowCount * colCount)
+        {
+            for (int i = maxRow; i >= minRow; i--)
+            {
+                matrix[i, maxCol] = num++;
+            }
+            maxCol--;
+        }
+        if (num <= rowCount * colCount)
+        {
+            for (int i = maxCol; i >= minCol; i--)
+            {
+                matrix[minRow, i] = num++;
+            }
+            minRow++;
+        }
+    }
+}
+
+Console.WriteLine("Введіть n-розмірність матриці: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введіть m-розмірність матриці: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+Console.WriteLine("Введіть 1, щоб рухатись за годинниковою стрілкою, або -1 - щоб проти: ");
+int direction = Convert.ToInt32(Console.ReadLine());
+if(direction==1)
+{
+    Console.WriteLine("За годинниковою: ");
+    FillMatrixInClockOrder(matrix);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+if (direction == -1)
+{
+    Console.WriteLine("Проти годинникової: ");
+    FillMatrixInAntiClockOrder(matrix);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+    
---
+++ Home_task_1/Task1_2/Task1_2/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Task1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[,] { 
+                { 1, 1, 1, 2, 3 }, 
+                { 1, 2, 3, 3, 3 }, 
+                { 2, 2, 2, 3, 3 }, 
+                { 3, 3, 3, 3, 4 } };
+
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            int maxLineLength = 0; 
+            int startRow = 0; 
+            int startCol = 0; 
+
+            for (int i = 0; i < rows; i++)
+            {
+                int currentLength = 1;
+                for (int j = 1; j < columns; j++)
+                {
+                    if (matrix[i, j] == matrix[i, j - 1]) 
+                    {
+                        currentLength++;
+                    }
+                    else 
+                    {
+                        if (currentLength > maxLineLength) 
+                        {
+                            maxLineLength = currentLength;
+                            startRow = i;
+                            startCol = j - currentLength;
+                        }
+                        currentLength = 1;
+                    }
+                }
+                if (currentLength > maxLineLength) 
+                {
+                    maxLineLength = currentLength;
+                    startRow = i;
+                    startCol = columns - currentLength;
+                }
+            }
+
+            Console.WriteLine("Знайдена найдовша лінія:");
+            Console.WriteLine("Початок: ({0}, {1})", startRow, startCol);
+            Console.WriteLine("Кінець: ({0}, {1})", startRow, startCol + maxLineLength - 1);
+            Console.WriteLine("Довжина: {0}", maxLineLength);
+        }
+    }
+}
